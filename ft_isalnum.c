5e33d17c19eea2f1fe99eaa140f900537ea94d15
Alex Gheorghe Calin Blejdea <acalin-b@c1r11s3.42madrid.com>
2023-03-09 17:24:05 +0100

    rename function name

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,11 +6,11 @@
 /*   By: acalin-b <acalin-b@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 17:14:19 by acalin-b          #+#    #+#             */
-/*   Updated: 2023/03/08 19:54:26 by acalin-b         ###   ########.fr       */
+/*   Updated: 2023/03/09 16:32:16 by acalin-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	isalnum(int c)
+int	ft_isalnum(int c)
 {
 	if (c >= '0' && c <= '9')
 		return (1);
