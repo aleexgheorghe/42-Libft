8718213290b557a7405b8d60e0199f294b7613d7
Alex Gheorghe Calin Blejdea <acalin-b@c1r11s3.42madrid.com>
2023-03-09 17:24:30 +0100

    added libft.h! Working :D

---
+++ libft.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acalin-b <acalin-b@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/09 15:14:45 by acalin-b          #+#    #+#             */
+/*   Updated: 2023/03/09 17:11:56 by acalin-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+int					ft_atoi(const char *str);
+int					ft_isalpha(int c);
+int					ft_isdigit(int c);
+int					ft_isalnum(int c);
+int					ft_isascii(int c);
+int					ft_isprint(int c);
+
+#endif
