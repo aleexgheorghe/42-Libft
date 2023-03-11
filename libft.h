6ece997477511d2706314b789408e49a46a01036
Alex Gheorghe Calin Blejdea <acalin-b@c1r11s3.42madrid.com>
2023-03-11 10:25:06 +0100

    update libft.h

--- libft.h
+++ libft.h
@@ -6,12 +6,13 @@
 /*   By: acalin-b <acalin-b@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 15:14:45 by acalin-b          #+#    #+#             */
-/*   Updated: 2023/03/09 17:11:56 by acalin-b         ###   ########.fr       */
+/*   Updated: 2023/03/10 13:57:00 by acalin-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
+# include <stddef.h>
 
 int					ft_atoi(const char *str);
 int					ft_isalpha(int c);
@@ -19,5 +20,10 @@ int					ft_isdigit(int c);
 int					ft_isalnum(int c);
 int					ft_isascii(int c);
 int					ft_isprint(int c);
+int					ft_strlen(const char *c);
+int					ft_strlcpy(char *dest, const char *src, size_t destsize);
+int					ft_toupper(int c);
+int					ft_tolower(int c);
+void				ft_memset(void *s, size_t n);
 
 #endif
