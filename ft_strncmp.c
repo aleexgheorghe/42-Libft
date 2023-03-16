3dd8efef7f89dc2d793122e634f86e4514ddae24
Alex Gheorghe Calin Blejdea <acalin-b@c1r11s3.42madrid.com>
2023-03-16 12:43:59 +0100

    added ft_strncmp.c

---
+++ ft_strncmp.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acalin-b <acalin-b@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/16 12:15:23 by acalin-b          #+#    #+#             */
+/*   Updated: 2023/03/16 12:36:53 by acalin-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *str1, const char *str2, size_t num)
+{
+	const unsigned char	*s1;
+	const unsigned char	*s2;
+
+	s1 = str1;
+	s2 = str2;
+	while (num-- && (*s1 || *s2))
+	{
+		if (*s1 != *s2 && *s1 > *s2)
+			return (1);
+		else if (*s1 != *s2 && *s1 < *s2)
+			return (-1);
+		else if (*s1 == *s2)
+		{
+			s1++;
+			s2++;
+		}
+	}
+	return (0);
+}
