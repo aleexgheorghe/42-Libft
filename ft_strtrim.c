9dca56db248f69845870cb101d675aebd9d8021c
Alex Gheorghe Calin Blejdea <acalin-b@c1r11s3.42madrid.com>
2023-03-19 18:10:58 +0100

    fix minor bugs and added new function strtim

---
+++ ft_strtrim.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acalin-b <acalin-b@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/19 12:56:11 by acalin-b          #+#    #+#             */
+/*   Updated: 2023/03/19 14:11:49 by acalin-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*str;
+	int		i;
+	int		j;
+	int		k;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	if (!s1 || !set)
+		return (NULL);
+	while (s1[i] && ft_strchr(set, s1[i]))
+		i++;
+	while (s1[j])
+		j++;
+	j--;
+	while (j > i && ft_strchr(set, s1[j]))
+		j--;
+	str = (char *)malloc(sizeof(char) * (j - i + 2));
+	if (!str)
+		return (NULL);
+	while (i <= j)
+		str[k++] = s1[i++];
+	str[k] = '\0';
+	return (str);
+}
+
+//int	main(void)
+//{
+//	char const *s1 = "Hello World";
+//	char const *set = "e";
+//
+//	printf("RETURNED: %s", ft_strtrim(s1, set));
+//
+//	return (0);
+//}
