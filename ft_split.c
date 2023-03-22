c439ce2d20dd1bd05e68972d322829957b533e24
Alex Gheorghe Calin Blejdea <acalin-b@c1r11s3.42madrid.com>
2023-03-22 11:31:50 +0100

    add base split

---
+++ ft_split.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acalin-b <acalin-b@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/22 10:01:21 by acalin-b          #+#    #+#             */
+/*   Updated: 2023/03/22 11:31:22 by acalin-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char	**ft_split(char const *s, char c)
+{
+}
+/*
+int	main(void)
+{
+	char const	*s;
+	char		c;
+
+	s = "lorem ipsum dolor sit amet";
+	c = ' ';
+	printf("RETURNED: %s", ft_split(s, c));
+	return (0);
+}
+ */
