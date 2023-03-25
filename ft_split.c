e96a9c755ec2d50e90e448f993e309d5d07c6012
Alex Gheorghe Calin Blejdea <acalin-b@c1r11s3.42madrid.com>
2023-03-25 13:34:53 +0100

    ft_split, protected null

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: acalin-b <acalin-b@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 10:01:21 by acalin-b          #+#    #+#             */
-/*   Updated: 2023/03/23 14:50:34 by acalin-b         ###   ########.fr       */
+/*   Updated: 2023/03/25 13:33:45 by acalin-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,32 +45,44 @@ static int	get_word_len(char const *s, char c)
 	return (len);
 }
 
+char	**fill_arr(char const *s, char c, char **arr)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (*s)
+	{
+		if (*s != c)
+		{
+			arr[i] = (char *)malloc(sizeof(char) * (get_word_len(s, c) + 1));
+			if (!arr[i])
+				return (NULL);
+			j = 0;
+			while (*s && *s != c)
+				arr[i][j++] = *s++;
+			arr[i][j] = '\0';
+			i++;
+		}
+		else
+			s++;
+	}
+	arr[i] = NULL;
+	return (arr);
+}
+
 char	**ft_split(char const *s, char c)
 {
 	char	**arr;
-	int		i;
-	int		j;
 	int		words;
 
+	if (!s)
+		return (NULL);
 	words = count_words(s, c);
 	arr = (char **)malloc(sizeof(char *) * (words + 1));
 	if (!arr)
 		return (NULL);
-	i = 0;
-	while (i < words)
-	{
-		while (*s == c)
-			s++;
-		arr[i] = (char *)malloc(sizeof(char) * (get_word_len(s, c) + 1));
-		if (!arr[i])
-			return (NULL);
-		j = 0;
-		while (*s && *s != c)
-			arr[i][j++] = *s++;
-		arr[i][j] = '\0';
-		i++;
-	}
-	arr[i] = NULL;
+	arr = fill_arr(s, c, arr);
 	return (arr);
 }
 
